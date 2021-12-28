9480acaa2b8514b44a02fe399dcff730300ba3a8
Erik Pragt <erik.pragt@gmail.com>
2021-12-28 14:02:46 +1100

    imp: Added tests from PR #616 (code was fine)

--- src/test/java/com/github/javafaker/InternetPasswordTest.java
+++ src/test/java/com/github/javafaker/InternetPasswordTest.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertTrue;
 
-public class InternetPasswordTest {
+public class InternetPasswordTest extends AbstractFakerTest {
     @Test
     public void testPassword1000() {
         Faker faker = new Faker();
@@ -26,5 +26,41 @@ public class InternetPasswordTest {
             assertTrue(isPasswordContainsDigit);
             assertTrue(isPasswordContainsSpecialCharacter);
         }
+    }
+
+    @Test
+    public void passwordSpecial(){
+        boolean check = true;
+        for (int i=0;i<10;i++){
+            String password = faker.internet().password(8, 16, true, true, true);
+            Pattern specialCharacterPattern = Pattern.compile("[^a-zA-Z0-9]");
+            Matcher specialCharacterMatcher = specialCharacterPattern.matcher(password);
+            if(!specialCharacterMatcher.find()){
+                check = false;
+                break;
+            }
+
+        }
+        assertTrue(check);
+    }
+    @Test
+    public void passwordMix(){
+        boolean check = true;
+        for (int i=0;i<10;i++){
+            String password = faker.internet().password(8, 16, true, true, true);
+            Pattern specialCharacterPattern = Pattern.compile("[^a-zA-Z0-9]");
+            Matcher specialCharacterMatcher = specialCharacterPattern.matcher(password);
+            Pattern digitPattern = Pattern.compile("[0-9]");
+            Matcher digitMatcher = digitPattern.matcher(password);
+            if(!specialCharacterMatcher.find()){
+                check = false;
+                break;
+            }
+            if(!digitMatcher.find()){
+                check = false;
+                break;
+            }
+        }
+        assertTrue(check);
     }
 }
