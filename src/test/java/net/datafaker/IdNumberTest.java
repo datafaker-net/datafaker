cffa0ecc77925ba8dae7ad4c7756927c701a341f
Erik Pragt <erik.pragt@gmail.com>
2022-02-14 22:32:03 +1100

    imp(codecoverage): Added more test and fixed test which wasn't testing the right thing.

--- src/test/java/net/datafaker/IdNumberTest.java
+++ src/test/java/net/datafaker/IdNumberTest.java
@@ -21,14 +21,14 @@ public class IdNumberTest extends AbstractFakerTest {
 
     @Test
     public void testSsnValid() {
-        assertThat(faker.idNumber().valid(), matchesRegularExpression("[0-8]\\d{2}-\\d{2}-\\d{4}"));
+        assertThat(faker.idNumber().ssnValid(), matchesRegularExpression("[0-8]\\d{2}-\\d{2}-\\d{4}"));
     }
 
     @Test
     public void testValidSwedishSsn() {
         final Faker f = new Faker(new Locale("sv_SE"));
         for (int i = 0; i < 100; i++) {
-            assertThat(f.idNumber().valid(), matchesRegularExpression("\\d{6}[-+]\\d{4}"));
+            assertThat(f.idNumber().validSvSeSsn(), matchesRegularExpression("\\d{6}[-+]\\d{4}"));
         }
     }
 
@@ -36,7 +36,23 @@ public class IdNumberTest extends AbstractFakerTest {
     public void testInvalidSwedishSsn() {
         final Faker f = new Faker(new Locale("sv_SE"));
         for (int i = 0; i < 100; i++) {
-            assertThat(f.idNumber().invalid(), matchesRegularExpression("\\d{6}[-+]\\d{4}"));
+            assertThat(f.idNumber().invalidSvSeSsn(), matchesRegularExpression("\\d{6}[-+]\\d{4}"));
+        }
+    }
+
+    @Test
+    public void testValidEnZaSsn() {
+        final Faker f = new Faker(new Locale("en_ZA"));
+        for (int i = 0; i < 100; i++) {
+            assertThat(f.idNumber().validEnZaSsn(), matchesRegularExpression("[0-9]{10}(0|1)8[0-9]"));
+        }
+    }
+
+    @Test
+    public void testInvalidEnZaSsn() {
+        final Faker f = new Faker(new Locale("en_ZA"));
+        for (int i = 0; i < 100; i++) {
+            assertThat(f.idNumber().inValidEnZaSsn(), matchesRegularExpression("[0-9]{10}(0|1)8[0-9]"));
         }
     }
 }
