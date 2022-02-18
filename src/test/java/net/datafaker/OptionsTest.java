81b56e39d669a543657d4342c69434ad14752350
Sergey Nuyanzin <snuyanzin@gmail.com>
2022-02-18 17:46:53 +0100

    Add tests for options

--- src/test/java/net/datafaker/OptionsTest.java
+++ src/test/java/net/datafaker/OptionsTest.java
@@ -3,6 +3,8 @@ package net.datafaker;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
 
@@ -31,9 +33,25 @@ public class OptionsTest extends AbstractFakerTest {
     }
 
     @Test
-    public void testOptionWithVarargsInteger() {
+    public void testOptionWithVarargs() {
         Integer[] integerOptions = new Integer[]{1, 3, 4, 5};
         assertThat(faker.options().option(1, 3, 4, 5), is(oneOf(integerOptions)));
+        Long[] longOptions = new Long[]{1L, 3L, 4L, 5L};
+        assertThat(faker.options().option(longOptions), is(oneOf(longOptions)));
+        Short[] shortOptions = new Short[]{1, 3, 4};
+        assertThat(faker.options().option(shortOptions), is(oneOf(shortOptions)));
+        Byte[] byteOptions = new Byte[]{(byte)11, (byte)13, (byte)14};
+        assertThat(faker.options().option(byteOptions), is(oneOf(byteOptions)));
+        Double[] doubleOptions = new Double[]{1.1d, 13d, 14.2d};
+        assertThat(faker.options().option(doubleOptions), is(oneOf(doubleOptions)));
+        Float[] floatOptions = new Float[]{1.2f, 13f, 14.2f};
+        assertThat(faker.options().option(floatOptions), is(oneOf(floatOptions)));
+        BigInteger[] bigIntegerOptions = new BigInteger[]{BigInteger.ONE, BigInteger.TEN, BigInteger.ZERO};
+        assertThat(faker.options().option(bigIntegerOptions), is(oneOf(bigIntegerOptions)));
+        BigDecimal[] bigDecimalOptions = new BigDecimal[]{BigDecimal.ONE, BigDecimal.TEN, BigDecimal.ZERO};
+        assertThat(faker.options().option(bigDecimalOptions), is(oneOf(bigDecimalOptions)));
+        Boolean[] booleanOptions = new Boolean[]{true, false};
+        assertThat(faker.options().option(booleanOptions), is(oneOf(booleanOptions)));
     }
 
     @Test
