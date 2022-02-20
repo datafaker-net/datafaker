bfde29994990dfc47b7084f3b866212042471d5e
zolv <zolv@users.noreply.github.com>
2022-02-20 18:31:59 +0100

    chore: code format

--- src/main/java/net/datafaker/IdNumber.java
+++ src/main/java/net/datafaker/IdNumber.java
@@ -14,132 +14,134 @@ import net.datafaker.idnumbers.SvSEIdNumber;
 import net.datafaker.idnumbers.ZhCnIdNumber;
 
 public class IdNumber {
-  private final Faker faker;
+    private final Faker faker;
 
-  protected IdNumber(Faker faker) {
-    this.faker = faker;
-  }
+    protected IdNumber(Faker faker) {
+        this.faker = faker;
+    }
 
-  public String valid() {
-    return faker.fakeValuesService().resolve("id_number.valid", this, faker);
-  }
+    public String valid() {
+        return faker.fakeValuesService().resolve("id_number.valid", this, faker);
+    }
 
-  public String invalid() {
-    return faker.numerify(faker.fakeValuesService().resolve("id_number.invalid", this, faker));
-  }
+    public String invalid() {
+        return faker.numerify(faker.fakeValuesService().resolve("id_number.invalid", this, faker));
+    }
 
-  public String ssnValid() {
-    EnIdNumber enIdNumber = new EnIdNumber();
-    return enIdNumber.getValidSsn(faker);
-  }
+    public String ssnValid() {
+        EnIdNumber enIdNumber = new EnIdNumber();
+        return enIdNumber.getValidSsn(faker);
+    }
 
     /**
      * Specified as #{IDNumber.valid_sv_se_ssn} in sv-SE.yml
      */
-  public String validSvSeSsn() {
-    SvSEIdNumber svSEIdNumber = new SvSEIdNumber();
+    public String validSvSeSsn() {
+        SvSEIdNumber svSEIdNumber = new SvSEIdNumber();
 
-    return svSEIdNumber.getValidSsn(faker);
-  }
+        return svSEIdNumber.getValidSsn(faker);
+    }
 
     /**
      * Specified as #{IDNumber.valid_en_za_ssn} in en-ZA.yml
      */
-  public String validEnZaSsn() {
-    EnZAIdNumber enZAIdNumber = new EnZAIdNumber();
-    return enZAIdNumber.getValidSsn(faker);
-  }
+    public String validEnZaSsn() {
+        EnZAIdNumber enZAIdNumber = new EnZAIdNumber();
+        return enZAIdNumber.getValidSsn(faker);
+    }
 
     /**
      * Specified as #{IDNumber.invalid_en_za_ssn} in en-ZA.yml
      */
-  public String inValidEnZaSsn() {
-    EnZAIdNumber enZAIdNumber = new EnZAIdNumber();
-    return enZAIdNumber.getInValidSsn(faker);
-  }
+    public String inValidEnZaSsn() {
+        EnZAIdNumber enZAIdNumber = new EnZAIdNumber();
+        return enZAIdNumber.getInValidSsn(faker);
+    }
 
     /**
      * Specified as #{IDNumber.invalid_sv_se_ssn} in sv-SE.yml
      */
-  public String invalidSvSeSsn() {
-    SvSEIdNumber svSEIdNumber = new SvSEIdNumber();
-    return svSEIdNumber.getInvalidSsn(faker);
-  }
+    public String invalidSvSeSsn() {
+        SvSEIdNumber svSEIdNumber = new SvSEIdNumber();
+        return svSEIdNumber.getInvalidSsn(faker);
+    }
 
-  public String singaporeanFin() {
-    return NricNumber.getValidFIN(faker, Type.FOREIGNER_TWENTY_FIRST_CENTURY);
-  }
+    public String singaporeanFin() {
+        return NricNumber.getValidFIN(faker, Type.FOREIGNER_TWENTY_FIRST_CENTURY);
+    }
 
-  public String singaporeanFinBefore2000() {
-    return NricNumber.getValidFIN(faker, Type.FOREIGNER_TWENTIETH_CENTURY);
-  }
+    public String singaporeanFinBefore2000() {
+        return NricNumber.getValidFIN(faker, Type.FOREIGNER_TWENTIETH_CENTURY);
+    }
 
-  public String singaporeanUin() {
-    return NricNumber.getValidFIN(faker, Type.SINGAPOREAN_TWENTY_FIRST_CENTURY);
-  }
+    public String singaporeanUin() {
+        return NricNumber.getValidFIN(faker, Type.SINGAPOREAN_TWENTY_FIRST_CENTURY);
+    }
 
-  public String singaporeanUinBefore2000() {
-    return NricNumber.getValidFIN(faker, Type.SINGAPOREAN_TWENTIETH_CENTURY);
-  }
+    public String singaporeanUinBefore2000() {
+        return NricNumber.getValidFIN(faker, Type.SINGAPOREAN_TWENTIETH_CENTURY);
+    }
 
-  /**
-   * Generate a valid Zh-CN id number.
-   *
-   * @return A Zh-CN id number
-   */
-  public String validZhCNSsn() {
-    ZhCnIdNumber zhCnIdNumber = new ZhCnIdNumber();
-    return zhCnIdNumber.getValidSsn(faker);
-  }
+    /**
+     * Generate a valid Zh-CN id number.
+     *
+     * @return A Zh-CN id number
+     */
+    public String validZhCNSsn() {
+        ZhCnIdNumber zhCnIdNumber = new ZhCnIdNumber();
+        return zhCnIdNumber.getValidSsn(faker);
+    }
 
-  public String validPtNif() {
-    PtNifIdNumber idNumber = new PtNifIdNumber();
-    return idNumber.getValid(faker);
-  }
+    public String validPtNif() {
+        PtNifIdNumber idNumber = new PtNifIdNumber();
+        return idNumber.getValid(faker);
+    }
 
-  public String invalidPtNif() {
-    PtNifIdNumber idNumber = new PtNifIdNumber();
-    return idNumber.getInvalid(faker);
-  }
+    public String invalidPtNif() {
+        PtNifIdNumber idNumber = new PtNifIdNumber();
+        return idNumber.getInvalid(faker);
+    }
 
-  /**
-   * Specified as #{IDNumber.valid_es_mx_ssn} in es-MX.yml
-   *
-   * @return A valid MEX CURP.
-   */
-  public String validEsMXSsn() {
-    EsMXIdNumber esMXIdNumber = new EsMXIdNumber();
-    return esMXIdNumber.get(faker);
-  }
 
-  /**
-   * Specified as #{IDNumber.invalid_es_mx_ssn} in es-MX.yml
-   *
-   * @return A valid MEX CURP.
-   */
-  public String invalidEsMXSsn() {
-    EsMXIdNumber esMXIdNumber = new EsMXIdNumber();
-    return esMXIdNumber.getWrong(faker);
-  }
+    /**
+     * Specified as #{IDNumber.valid_es_mx_ssn} in es-MX.yml
+     *
+     * @return A valid MEX CURP.
+     */
+    public String validEsMXSsn() {
+        EsMXIdNumber esMXIdNumber = new EsMXIdNumber();
+        return esMXIdNumber.get(faker);
+    }
 
-  /**
-   * Generates a valid PESEL number for a person of random gender and age between 0 and 100.
-   *
-   * @return A valid PESEL number
-   */
-  public String peselNumber() {
-    return peselNumber(
-        faker.date().birthday(0, 100).toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
-        Gender.ANY);
-  }
+    /**
+     * Specified as #{IDNumber.invalid_es_mx_ssn} in es-MX.yml
+     *
+     * @return A valid MEX CURP.
+     */
+    public String invalidEsMXSsn() {
+        EsMXIdNumber esMXIdNumber = new EsMXIdNumber();
+        return esMXIdNumber.getWrong(faker);
+    }
 
-  /**
-   * Generates a valid PESEL number for a person with given gender and birth date.
-   * @param birthDate Given birth date
-   * @param gender Person's gender. Null value means {@link Gender.ANY}
-   * @return A valid PESEL number
-   */
-  public String peselNumber(LocalDate birthDate, Gender gender) {
-    return new PeselNumber(faker).get(birthDate, gender);
-  }
+    /**
+     * Generates a valid PESEL number for a person of random gender and age between
+     * 0 and 100.
+     *
+     * @return A valid PESEL number
+     */
+    public String peselNumber() {
+	return peselNumber(faker.date().birthday(0, 100).toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
+		Gender.ANY);
+    }
+
+    /**
+     * Generates a valid PESEL number for a person with given gender and birth date.
+     *
+     * @param birthDate Given birth date
+     * @param gender    Person's gender. Null value means {@link Gender.ANY}
+     * @return A valid PESEL number
+     */
+    public String peselNumber(LocalDate birthDate, Gender gender) {
+	return new PeselNumber(faker).get(birthDate, gender);
+    }
 }
