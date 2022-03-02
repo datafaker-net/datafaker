a30cc61eb42e79d10dfa00257254ce468cfeeaae
Erik Pragt <erik.pragt@gmail.com>
2022-03-02 21:17:51 +1100

    fix(test): Changed expectation since the tool test was failing because a 3D scanner was created.

--- src/test/java/net/datafaker/ScienceTest.java
+++ src/test/java/net/datafaker/ScienceTest.java
@@ -1,43 +1,43 @@
 package net.datafaker;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import static net.datafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ScienceTest extends AbstractFakerTest {
 
-    @Test
+    @RepeatedTest(10)
     public void element() {
         assertThat(faker.science().element(), matchesRegularExpression("[A-Za-z ]+"));
     }
 
-    @Test
+    @RepeatedTest(10)
     public void elementSymbol() {
         assertThat(faker.science().elementSymbol(), matchesRegularExpression("[A-Za-z]{1,2}"));
     }
 
-    @Test
+    @RepeatedTest(10)
     public void scientist() {
         assertThat(faker.science().scientist(), matchesRegularExpression("[A-Za-z. -]+"));
     }
 
-    @Test
+    @RepeatedTest(10)
     public void tool() {
-        assertThat(faker.science().tool(), matchesRegularExpression("[A-Za-z. -]+"));
+        assertThat(faker.science().tool(), matchesRegularExpression("[0-9A-Za-z. -]+"));
     }
 
-    @Test
+    @RepeatedTest(10)
     public void quark() {
         assertThat(faker.science().quark(), matchesRegularExpression("[A-Za-z]+"));
     }
 
-    @Test
+    @RepeatedTest(10)
     public void leptons() {
         assertThat(faker.science().leptons(), matchesRegularExpression("[A-Za-z ]+"));
     }
 
-    @Test
+    @RepeatedTest(10)
     public void bosons() {
         assertThat(faker.science().bosons(), matchesRegularExpression("[A-Za-z ]+"));
     }
