3a5ab171e878c9e657a80ade0a6c838fdf93a0f5
Sergey Nuyanzin <snuyanzin@gmail.com>
2022-03-08 11:19:01 +0100

    Add toCsv for fake collections

--- src/test/java/net/datafaker/FakeCollectionTest.java
+++ src/test/java/net/datafaker/FakeCollectionTest.java
@@ -47,4 +47,110 @@ public class FakeCollectionTest extends AbstractFakerTest {
                 .maxLen(5).build().get());
         assertEquals("Max length must be not less than min length and not negative", iae.getMessage());
     }
+
+    private interface Data {
+        String name();
+
+        String value();
+
+        String range();
+
+        String unit();
+    }
+
+    private static class BloodPressure implements Data {
+
+        @Override
+        public String name() {
+            return "Mean Blood Pressure";
+        }
+
+        @Override
+        public String value() {
+            return faker.random().nextInt(60, 180) + "";
+        }
+
+        @Override
+        public String range() {
+            return "";
+        }
+
+        @Override
+        public String unit() {
+            return "mm Hg";
+        }
+    }
+
+    private static class Glucose implements Data {
+
+        @Override
+        public String name() {
+            return "Glucose";
+        }
+
+        @Override
+        public String value() {
+            return String.format("%.1f", faker.random().nextDouble(3.2, 5.5));
+        }
+
+        @Override
+        public String range() {
+            return "3.2-5.5";
+        }
+
+        @Override
+        public String unit() {
+            return "mmol/L";
+        }
+    }
+
+    private static class Temperature implements Data {
+
+        @Override
+        public String name() {
+            return "Temperature";
+        }
+
+        @Override
+        public String value() {
+            return faker.random().nextInt(30, 50) + "";
+        }
+
+        @Override
+        public String range() {
+            return "";
+        }
+
+        @Override
+        public String unit() {
+            return "degrees C";
+        }
+    }
+
+    @Test
+    public void myTest() {
+        String separator = "$$$";
+        int limit = 5;
+        String csv = new FakeCollection.Builder<Data>().minLen(limit).maxLen(limit)
+            .suppliers(BloodPressure::new, Glucose::new, Temperature::new)
+            .build()
+            .toCsv()
+            .headers(() -> "name", () -> "value", () -> "range", () -> "unit")
+            .columns(Data::name, Data::value, Data::range, Data::unit)
+            .separator(separator)
+            .build().get();
+
+        int numberOfLines = 0;
+        int numberOfSeparator = 0;
+        for (int i = 0; i < csv.length(); i++) {
+            if (csv.regionMatches(i, System.lineSeparator(), 0, System.lineSeparator().length())) {
+                numberOfLines++;
+            } else if (csv.regionMatches(i, separator, 0, separator.length())) {
+                numberOfSeparator++;
+            }
+        }
+
+        assertEquals(limit + 1, numberOfLines); // limit + 1 line for header
+        assertEquals((limit + 1) * (4 - 1), numberOfSeparator); // number of lines * (number of columns - 1)
+    }
 }
