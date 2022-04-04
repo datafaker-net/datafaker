e094184150597d8e58fd4af4534b0bb7b936e7cd
Sergey Nuyanzin <snuyanzin@gmail.com>
2022-04-04 09:37:47 +0200

    Add null rate for fake collections

--- src/main/java/net/datafaker/FakeCollection.java
+++ src/main/java/net/datafaker/FakeCollection.java
@@ -12,18 +12,23 @@ import java.util.function.Supplier;
 public class FakeCollection<T> {
     private final RandomService randomService;
     private final List<Supplier<T>> suppliers;
+    private final double nullRate;
     private final int minLength;
     private final int maxLength;
 
-    private FakeCollection(List<Supplier<T>> suppliers, int minLength, int maxLength, RandomService randomService) {
+    private FakeCollection(List<Supplier<T>> suppliers, int minLength, int maxLength, RandomService randomService, double nullRate) {
         this.suppliers = suppliers;
         this.minLength = minLength;
         this.maxLength = maxLength;
         this.randomService = randomService;
+        this.nullRate = nullRate;
     }
 
     public T singleton() {
-        return suppliers.get(randomService.nextInt(suppliers.size())).get();
+        if (nullRate == 0d || randomService.nextDouble() >= nullRate) {
+            return suppliers.get(randomService.nextInt(suppliers.size())).get();
+        }
+        return null;
     }
 
     public List<T> get() {
@@ -39,6 +44,7 @@ public class FakeCollection<T> {
         private final List<Supplier<T>> suppliers = new ArrayList<>();
         private int minLength = -1; // negative means same as maxLength
         private int maxLength = 10;
+        private double nullRate = 0d;
         private Faker faker;
 
         public Builder<T> faker(Faker faker) {
@@ -53,6 +59,14 @@ public class FakeCollection<T> {
 
         public Builder<T> maxLen(int maxLength) {
             this.maxLength = maxLength;
+            return this;
+        }
+
+        public Builder<T> nullRate(double nullRate) {
+            if (nullRate < 0 || nullRate > 1) {
+                throw new IllegalArgumentException("Null rate should be between 0 and 1");
+            }
+            this.nullRate = nullRate;
             return this;
         }
 
@@ -76,7 +90,7 @@ public class FakeCollection<T> {
                 randomService = faker.random();
             }
 
-            return new FakeCollection<>(suppliers, minLength, maxLength, randomService);
+            return new FakeCollection<>(suppliers, minLength, maxLength, randomService, nullRate);
         }
     }
 
