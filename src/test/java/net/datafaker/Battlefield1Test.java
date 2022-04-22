1beec719c662a133db2872beeb3e2e77974fc2a2
cooljj <1009839598@qq.com>
2022-04-22 22:42:12 +0800

    Update Test

--- src/test/java/net/datafaker/Battlefield1Test.java
+++ src/test/java/net/datafaker/Battlefield1Test.java
@@ -15,19 +15,19 @@ public class Battlefield1Test extends AbstractFakerTest {
     //CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
     @Test
     public void weapon() {
-        assertThat(faker.battlefield1().weapon()).matches("[A-Za-z0-9,\\.\\(\\)\\/ ]+");
+        assertThat(faker.battlefield1().weapon()).matches("[A-Za-z0-9,\\- .()/]+");
     }
 
     //CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
     @Test
     public void vehicle() {
-        assertThat(faker.battlefield1().vehicle()).matches("[A-Za-z0-9,\\-\\.\\(\\)\\/ ]+");
+        assertThat(faker.battlefield1().vehicle()).matches("[A-Za-z0-9,\\- .()/]+");
     }
 
     //CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
     @Test
     public void map() {
-        assertThat(faker.battlefield1().map()).matches("[A-Za-z0-9,\\-\\.\\(\\)\\' ]+");
+        assertThat(faker.battlefield1().map()).matches("[A-Za-z0-9,\\- .()']+");
     }
 
     //CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
