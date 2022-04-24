ab48559705240078456deeb275d3120261f0b748
Uncle-Road <457894974@qq.com>
2022-04-24 17:18:27 +0800

    fix some NAN case and test multiple

--- src/test/java/net/datafaker/WeaponTest.java
+++ src/test/java/net/datafaker/WeaponTest.java
@@ -1,46 +1,43 @@
 package net.datafaker;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 
 public class WeaponTest extends AbstractFakerTest {
 
 
-    @Test
+    @RepeatedTest(500)
     public void getWeaponTest() {
         assertThat(faker.weapon().getWeapon()).matches("[a-zA-Z0-9\\-\\.\\ \\/]+");
     }
 
 
-    @Test
+    @RepeatedTest(500)
     public void provenanceTest() {
         assertThat(faker.weapon().provenance()).matches("[a-zA-Z0-9\\-\\.\\ \\/]+");
     }
 
 
-    @Test
+    @RepeatedTest(500)
     public void userTest() {
         assertThat(faker.weapon().user()).matches("[a-zA-Z0-9\\-\\.\\ \\/]+");
     }
 
 
-    @Test
+    @RepeatedTest(500)
     public void functionTest() {
         assertThat(faker.weapon().function()).matches("[a-zA-Z0-9\\-\\.\\ \\/]+");
     }
 
 
-    @Test
+    @RepeatedTest(500)
     public void targetTest() {
         assertThat(faker.weapon().target()).matches("[a-zA-Z0-9\\-\\.\\ \\/]+");
     }
 
 
-    @Test
+    @RepeatedTest(500)
     public void typeTest() {
         assertThat(faker.weapon().type()).matches("[a-zA-Z0-9\\-\\.\\ \\/]+");
     }
