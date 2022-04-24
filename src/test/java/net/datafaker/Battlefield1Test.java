e17fe8b0f8cd244ba6a67457140574c272df1e90
cooljj <1009839598@qq.com>
2022-04-24 16:15:05 +0800

    Update repeated tests

--- src/test/java/net/datafaker/Battlefield1Test.java
+++ src/test/java/net/datafaker/Battlefield1Test.java
@@ -1,37 +1,33 @@
 package net.datafaker;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Battlefield1Test extends AbstractFakerTest {
 
-    //CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
-    @Test
+    @RepeatedTest(50)
     public void classes() {
         assertThat(faker.battlefield1().classes()).matches("[A-Za-z ]+");
     }
 
-    //CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
-    @Test
+    @RepeatedTest(200)
     public void weapon() {
         assertThat(faker.battlefield1().weapon()).matches("[A-Za-z0-9,\\- .()/]+");
     }
 
-    //CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
-    @Test
+    @RepeatedTest(200)
     public void vehicle() {
         assertThat(faker.battlefield1().vehicle()).matches("[A-Za-z0-9,\\- .()/]+");
     }
 
-    //CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
-    @Test
+    @RepeatedTest(100)
     public void map() {
         assertThat(faker.battlefield1().map()).matches("[A-Za-z0-9,\\- .()']+");
     }
 
-    //CS304 (manually written) Issue link: https://github.com/DiUS/java-faker/issues/711
-    @Test
+    @RepeatedTest(20)
     public void faction() {
         assertThat(faker.battlefield1().faction()).matches("[A-Za-z,\\- ]+");
     }
