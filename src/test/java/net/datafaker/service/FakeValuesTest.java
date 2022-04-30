8ddcc64fa9ae6de77c036d22432e372aec7fafeb
Sergey Nuyanzin <snuyanzin@gmail.com>
2022-04-30 17:54:30 +0200

    Add tests for equal/not equal FakeValues

--- src/test/java/net/datafaker/service/FakeValuesTest.java
+++ src/test/java/net/datafaker/service/FakeValuesTest.java
@@ -2,10 +2,17 @@ package net.datafaker.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Locale;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 
 public class FakeValuesTest {
@@ -56,4 +63,28 @@ public class FakeValuesTest {
         assertThat(fakeValues.get(PATH)).isNull();
     }
 
+    @ParameterizedTest
+    @MethodSource("fakeValuesProvider")
+    void checkEquals(FakeValues fv1, FakeValues fv2, boolean equals) {
+        if (equals) {
+            assertThat(fv1).isEqualTo(fv2);
+        } else {
+            assertThat(fv1).isNotEqualTo(fv2);
+        }
+    }
+
+    static Stream<Arguments> fakeValuesProvider() {
+        Path tmp = Paths.get("tmp");
+        return Stream.of(
+            of(new FakeValues(Locale.CANADA), new FakeValues(Locale.CANADA), true),
+            of(null, new FakeValues(Locale.CANADA), false),
+            of(new FakeValues(Locale.CANADA), null, false),
+            of(new FakeValues(Locale.CANADA), null, false),
+            of(new FakeValues(Locale.ENGLISH), new FakeValues(Locale.ENGLISH, "filepath", "path"), false),
+            of(new FakeValues(Locale.ENGLISH, "filepath", null), new FakeValues(Locale.ENGLISH, "filepath", "path"), false),
+            of(new FakeValues(Locale.ENGLISH, "filepath", "path"), new FakeValues(Locale.ENGLISH, "filepath", "path"), true),
+            of(new FakeValues(Locale.ENGLISH, "filepath", "path", tmp), new FakeValues(Locale.ENGLISH, "filepath", "path", tmp), true),
+            of(new FakeValues(Locale.ENGLISH, "filepath", "path", Paths.get("tmp2")), new FakeValues(Locale.ENGLISH, "filepath", "path", tmp), false)
+        );
+    }
 }
