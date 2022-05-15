d549f57b0da9b98712693742c2aeefdb2bfb28a9
Sergey Nuyanzin <snuyanzin@gmail.com>
2022-05-15 20:45:13 +0200

    Replace regex with cycle for javaNameToYamlName

--- src/test/java/net/datafaker/ElectricalComponentsTest.java
+++ src/test/java/net/datafaker/ElectricalComponentsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class ElectricalComponentsTest extends AbstractFakerTest{
+class ElectricalComponentsTest extends AbstractFakerTest {
 
     @Test
     void testActive() {
