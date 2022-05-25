36f3b8589099a3efbb7c9c8536ceddd25a1ff8d9
Erik Pragt <erik.pragt@gmail.com>
2022-05-25 20:29:25 +1000

    imp(verbs): Added - to allowed characters

--- src/test/java/net/datafaker/VerbTest.java
+++ src/test/java/net/datafaker/VerbTest.java
@@ -1,33 +1,35 @@
 package net.datafaker;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class VerbTest extends AbstractFakerTest {
 
-    @Test
+    public static final String WORDS = "[\\w-]+";
+
+    @RepeatedTest(10)
     void testBase() {
-        assertThat(faker.verb().base()).matches("\\w+");
+        assertThat(faker.verb().base()).matches(WORDS);
     }
 
-    @Test
+    @RepeatedTest(10)
     void testPast() {
-        assertThat(faker.verb().past()).matches("\\w+");
+        assertThat(faker.verb().past()).matches(WORDS);
     }
 
-    @Test
+    @RepeatedTest(10)
     void testPastParticiple() {
-        assertThat(faker.verb().pastParticiple()).matches("\\w+");
+        assertThat(faker.verb().pastParticiple()).matches(WORDS);
     }
 
-    @Test
+    @RepeatedTest(10)
     void testSimplePresent() {
-        assertThat(faker.verb().simplePresent()).matches("\\w+");
+        assertThat(faker.verb().simplePresent()).matches(WORDS);
     }
 
-    @Test
+    @RepeatedTest(10)
     void testIngForm() {
-        assertThat(faker.verb().ingForm()).matches("\\w+");
+        assertThat(faker.verb().ingForm()).matches(WORDS);
     }
 }
