714a7779ff11259c471e50bd0e8cf41bdc35e6a5
SickDawn <68135258+SickDawn@users.noreply.github.com>
2022-05-29 03:10:52 +0800

    Update DarkSoulTest.java

--- src/test/java/net/datafaker/DarkSoulTest.java
+++ src/test/java/net/datafaker/DarkSoulTest.java
@@ -8,23 +8,23 @@ class DarkSouleTest extends AbstractFakerTest {
 
     @Test
     void testClasses() {
-        assertThat(faker.darkSoul().classes()).matches("[A-Za-z]+");
+        assertThat(faker.darkSoul().classes()).matches("[A-Za-z ']+");
     }
 
     @Test
     void testCovenants() {
-        assertThat(faker.darkSoul().covenants()).matches("[A-Za-z ]+");
+        assertThat(faker.darkSoul().covenants()).matches("[A-Za-z ']+");
     }
     @Test
     void testWeapon() {
-        assertThat(faker.darkSoul().weapon()).matches("[A-Za-z ]+");
+        assertThat(faker.darkSoul().weapon()).matches("[A-Za-z ']+");
     }
     @Test
     void testShield() {
-        assertThat(faker.darkSoul().shield()).matches("[A-Za-z ]+");
+        assertThat(faker.darkSoul().shield()).matches("[A-Za-z ']+");
     }
     @Test
     void testStats() {
-        assertThat(faker.darkSoul().stats()).matches("[A-Za-z ]+");
+        assertThat(faker.darkSoul().stats()).matches("[A-Za-z ']+");
     }
 }
