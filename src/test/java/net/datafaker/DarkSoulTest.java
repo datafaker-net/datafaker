e30044762641dfadd53d5bdb6a5d293f4fd7e6c9
Erik Pragt <erik.pragt@gmail.com>
2022-05-29 23:11:43 +1000

    fix(dark_soul): Fixed test

--- src/test/java/net/datafaker/DarkSoulTest.java
+++ src/test/java/net/datafaker/DarkSoulTest.java
@@ -6,23 +6,25 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class DarkSoulTest extends AbstractFakerTest {
 
+    public static final String DARK_SOUL_REGEX = "[A-Za-z '-]+";
+
     @RepeatedTest(10)
     void testClasses() {
-        assertThat(faker.darkSoul().classes()).matches("[A-Za-z ']+");
+        assertThat(faker.darkSoul().classes()).matches(DARK_SOUL_REGEX);
     }
 
     @RepeatedTest(10)
     void testCovenants() {
-        assertThat(faker.darkSoul().covenants()).matches("[A-Za-z ']+");
+        assertThat(faker.darkSoul().covenants()).matches(DARK_SOUL_REGEX);
     }
 
     @RepeatedTest(10)
     void testShield() {
-        assertThat(faker.darkSoul().shield()).matches("[A-Za-z ']+");
+        assertThat(faker.darkSoul().shield()).matches(DARK_SOUL_REGEX);
     }
 
     @RepeatedTest(10)
     void testStats() {
-        assertThat(faker.darkSoul().stats()).matches("[A-Za-z ']+");
+        assertThat(faker.darkSoul().stats()).matches(DARK_SOUL_REGEX);
     }
 }
