fe972f7409babe5a2d085a9b427fcb8b23363423
Sergey Nuyanzin <snuyanzin@gmail.com>
2022-06-24 21:44:19 +0200

    Add fast return if a string is already capitalized

--- src/main/java/net/datafaker/internal/helper/WordUtils.java
+++ src/main/java/net/datafaker/internal/helper/WordUtils.java
@@ -7,6 +7,7 @@ public class WordUtils {
     public static String capitalize(String input) {
         if (input == null) return null;
         if (input.equals("")) return "";
+        if (Character.isUpperCase(input.charAt(0))) return input;
         return input.substring(0, 1).toUpperCase(Locale.ROOT) + input.substring(1);
     }
 }
