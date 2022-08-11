116c73f07ab4ea816350f36d6ea1a739066d3376
Domingo Perez <d.perez@lookiero.com>
2022-08-11 13:17:39 +0100

    Refactor name test and method used

--- src/main/java/net/datafaker/GarmentSize.java
+++ src/main/java/net/datafaker/GarmentSize.java
@@ -17,60 +17,6 @@ public class GarmentSize extends AbstractProvider {
    * @return a string of garment size
    */
   public String size() {
-    return faker.fakeValuesService().fetchString("garments.sizes");
+    return faker.resolve("garments_sizes.sizes");
   }
-
-  /**
-   * This method returns a garment XS size
-   *
-   * @return a XS string of garment size
-   */
-  public String anXS() { return faker.fakeValuesService().fetchString("garments.xs"); }
-
-  /**
-   * This method returns a garment S size
-   *
-   * @return a S string of garment size
-   */
-  public String anS() { return faker.fakeValuesService().fetchString("garments.s"); }
-
-
-  /**
-   * This method returns a garment M size
-   *
-   * @return a M string of garment size
-   */
-  public String anM() { return faker.fakeValuesService().fetchString("garments.m"); }
-
-
-  /**
-   * This method returns a garment L size
-   *
-   * @return a L string of garment size
-   */
-  public String anL() { return faker.fakeValuesService().fetchString("garments.l"); }
-
-  /**
-   * This method returns a garment XL size
-   *
-   * @return a XL string of garment size
-   */
-  public String anXL() { return faker.fakeValuesService().fetchString("garments.xl"); }
-
-  /**
-   * This method returns a garment XXL size
-   *
-   * @return a XXL string of garment size
-   */
-  public String anXXL() { return faker.fakeValuesService().fetchString("garments.xxl"); }
-
-
-  /**
-   * This method returns a garment XXXL size
-   *
-   * @return a XXXL string of garment size
-   */
-  public String anXXXL() { return faker.fakeValuesService().fetchString("garments.xxxl"); }
-
-
 }
--- src/test/java/net/datafaker/GarmentSizeTest.java
+++ src/test/java/net/datafaker/GarmentSizeTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GarmentSizeTest extends AbstractFakerTest {
 
   @Test
-  public void sizes() {
-    assertThat(faker.garmentSize().size()).matches("([A-Z]+)");
+  public void size() {
+    assertThat(faker.garmentSize().size()).isNotEmpty();
   }
 }
