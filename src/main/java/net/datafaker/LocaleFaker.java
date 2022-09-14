543e62f7140a58c84e2c2b6719b29d6a6f717c8b
Ilya Pantsyr <panilyau@gmail.com>
2022-09-14 14:51:12 +0300

    Refactor LocaleFaker.displayName in order to avoid empty outputs

--- src/main/java/net/datafaker/LocaleFaker.java
+++ src/main/java/net/datafaker/LocaleFaker.java
@@ -1,12 +1,18 @@
 package net.datafaker;
 
+import net.datafaker.service.LocalePicker;
+import net.datafaker.service.RandomService;
+
 import java.text.DateFormat;
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class LocaleFaker extends AbstractProvider {
 
     private final Locale[] availableLocales = DateFormat.getAvailableLocales();
-    private final Locale locale = availableLocales[faker.random().nextInt(availableLocales.length)];
 
     protected LocaleFaker(Faker faker) {
         super(faker);
@@ -14,9 +20,10 @@ public class LocaleFaker extends AbstractProvider {
 
     /**
      *
-     * @return locale in form: "en-US"
+     * @return locale in form: "en_US"
      */
     public String baseLocale() {
+        final Locale locale = availableLocales[faker.random().nextInt(availableLocales.length)];
         return locale.toString();
     }
 
@@ -25,6 +32,11 @@ public class LocaleFaker extends AbstractProvider {
      * @return locale in form: "English (United States)"
      */
     public String displayName() {
+        int randomIndex = new RandomService().nextInt(availableLocales.length - 1);
+        Locale locale = Arrays.stream(availableLocales)
+            .filter(lcl -> !lcl.getDisplayName().isEmpty())
+            .skip(randomIndex).findFirst().get();
+
         return locale.getDisplayName();
     }
 }
--- src/test/java/net/datafaker/LocaleFakerTest.java
+++ src/test/java/net/datafaker/LocaleFakerTest.java
@@ -1,17 +1,17 @@
 package net.datafaker;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LocaleFakerTest extends AbstractFakerTest {
 
-    @Test
+    @RepeatedTest(10)
     void baseLocale() {
         assertThat(faker.localeFaker().baseLocale()).isNotEmpty();
     }
 
-    @Test
+    @RepeatedTest(10)
     void displayName() {
         assertThat(faker.localeFaker().displayName()).isNotEmpty();
     }
