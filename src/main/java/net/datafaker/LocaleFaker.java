419d878c19f563c795445e2eb59651c6107ded53
Ilya Pantsyr <panilyau@gmail.com>
2022-09-15 15:03:45 +0300

    Filter available locales only once instead of filtering every method call

--- src/main/java/net/datafaker/LocaleFaker.java
+++ src/main/java/net/datafaker/LocaleFaker.java
@@ -2,11 +2,15 @@ package net.datafaker;
 
 import java.text.DateFormat;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class LocaleFaker extends AbstractProvider {
 
-    private final Locale[] availableLocales = DateFormat.getAvailableLocales();
+    private final List<Locale> availableLocales = Arrays.stream(DateFormat.getAvailableLocales())
+        .filter(locale -> !locale.getDisplayName().isEmpty())
+        .collect(Collectors.toList());
 
     protected LocaleFaker(Faker faker) {
         super(faker);
@@ -17,20 +21,17 @@ public class LocaleFaker extends AbstractProvider {
      * @return locale in form: "en_US"
      */
     public String baseLocale() {
-        final Locale locale = availableLocales[faker.random().nextInt(availableLocales.length)];
+        final Locale locale = availableLocales.get(faker.random().nextInt(availableLocales.size()));
         return locale.toString();
     }
 
     /**
      *
-     * @return locale in form: "English (United States)"
+     * @return locale in form: "English (United States) or en_IN"
      */
     public String displayName() {
-        int randomIndex = faker.random().nextInt(availableLocales.length - 1);
-        Locale locale = Arrays.stream(availableLocales)
-            .filter(lcl -> !lcl.getDisplayName().isEmpty())
-            .skip(randomIndex).findFirst().get();
-
+        int randomIndex = faker.random().nextInt(availableLocales.size());
+        Locale locale = availableLocales.get(randomIndex);
         return locale.getDisplayName();
     }
 }
