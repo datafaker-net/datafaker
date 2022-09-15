0d7aa6fea8a93e5f4ec2d7d477e443446936445a
Michal Gutowski <michal@cucumbersome.net>
2022-09-15 15:46:57 +0200

    Make sure that padding is used in Hashing
    In some cases it's possible to have MD5 hash (and possibly other as well) with the first byte set to 0. In this case it gets removed, while converting to BigInteger and the hex string has 31 characters. To make sure that it works as expected, we need to guarantee proper padding.

--- src/main/java/net/datafaker/Hashing.java
+++ src/main/java/net/datafaker/Hashing.java
@@ -15,35 +15,35 @@ public class Hashing extends AbstractProvider {
     }
 
     public String md2() {
-        return generateString("MD2");
+        return generateString("MD2", "%032x");
     }
 
     public String md5() {
-        return generateString("MD5");
+        return generateString("MD5", "%032x");
     }
 
     public String sha1() {
-        return generateString("SHA-1");
+        return generateString("SHA-1", "%040x");
     }
 
     public String sha384() {
-        return generateString("SHA-384");
+        return generateString("SHA-384", "%096x");
     }
 
     public String sha256() {
-        return generateString("SHA-256");
+        return generateString("SHA-256", "%064x");
     }
 
     public String sha512() {
-        return generateString("SHA-512");
+        return generateString("SHA-512", "%0128x");
     }
 
-    private String generateString(String algorithm) {
+    private String generateString(String algorithm, String format) {
         try {
             MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
             String characters = faker.lorem().characters();
             messageDigest.update(characters.getBytes(StandardCharsets.UTF_8), 0, characters.length());
-            return new BigInteger(1, messageDigest.digest()).toString(16);
+            return String.format(format, new BigInteger(1, messageDigest.digest()));
         } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
             throw new RuntimeException(noSuchAlgorithmException);
         }
