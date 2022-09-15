822ae290e67d6d9a87a340def3526bb4ebe63eb3
Michał Gutowski <CucumisSativus@users.noreply.github.com>
2022-09-15 16:41:30 +0200

    Make sure that padding is used in Hashing (#360)
    In some cases it's possible to have MD5 hash (and possibly other as well) with the first byte set to 0. In this case it gets removed, while converting to BigInteger and the hex string has 31 characters. To make sure that it works as expected, we need to guarantee proper padding.

--- src/test/java/net/datafaker/HashingTest.java
+++ src/test/java/net/datafaker/HashingTest.java
@@ -8,31 +8,31 @@ class HashingTest extends AbstractFakerTest {
 
     @Test
     void testMd2() {
-        assertThat(faker.hashing().md2()).matches("[a-z\\d]+");
+        assertThat(faker.hashing().md2()).matches("\\b[a-fA-F\\d]{32}\\b");
     }
 
     @Test
     void testMd5() {
-        assertThat(faker.hashing().md5()).matches("[a-z\\d]+");
+        assertThat(faker.hashing().md5()).matches("\\b[a-fA-F\\d]{32}\\b");
     }
 
     @Test
     void testSha1() {
-        assertThat(faker.hashing().sha1()).matches("[a-z\\d]+");
+        assertThat(faker.hashing().sha1()).matches("\\b[a-fA-F\\d]{40}\\b");
     }
 
     @Test
     void testSha256() {
-        assertThat(faker.hashing().sha256()).matches("[a-z\\d]+");
+        assertThat(faker.hashing().sha256()).matches("\\b[a-fA-F\\d]{64}\\b");
     }
 
     @Test
     void testSha384() {
-        assertThat(faker.hashing().sha384()).matches("[a-z\\d]+");
+        assertThat(faker.hashing().sha384()).matches("\\b[a-fA-F\\d]{96}\\b");
     }
 
     @Test
     void testSha512() {
-        assertThat(faker.hashing().sha512()).matches("[a-z\\d]+");
+        assertThat(faker.hashing().sha512()).matches("\\b[a-fA-F\\d]{128}\\b");
     }
 }
