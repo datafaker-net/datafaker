03acaf5b3ab04ee822bb45aa5f45d84597c0ea71
Erik Pragt <erik.pragt@gmail.com>
2022-09-24 11:22:22 +1000

    Added Mazda as a car make and simplified BaseFakerTest.

--- src/test/java/net/datafaker/base/BaseFakerTest.java
+++ src/test/java/net/datafaker/base/BaseFakerTest.java
@@ -1,11 +1,8 @@
 package net.datafaker.base;
 
-import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.MockitoAnnotations;
 
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
@@ -13,20 +10,11 @@ import java.util.logging.Logger;
 
 public class BaseFakerTest<T extends BaseFaker> {
 
-    protected static final Function<Predicate<Character>, Condition<String>> CONDITION_FUNCTION = characterPredicate -> new Condition<>(s -> {
-        for (int i = 0; i < s.length(); i++) {
-            if (!characterPredicate.test(s.charAt(i))) {
-                return false;
-            }
-        }
-        return true;
-    }, "condition");
-
     protected final T faker = getFaker();
 
     @BeforeEach
     public void before() {
-        try (AutoCloseable ac = MockitoAnnotations.openMocks(this)) {
+        try (AutoCloseable ignored = MockitoAnnotations.openMocks(this)) {
 
             Logger rootLogger = LogManager.getLogManager().getLogger("");
             Handler[] handlers = rootLogger.getHandlers();
--- src/test/java/net/datafaker/movie/OscarMovieTest.java
+++ src/test/java/net/datafaker/movie/OscarMovieTest.java
@@ -1,7 +1,6 @@
 package net.datafaker.movie;
 
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Strings.isNullOrEmpty;
@@ -10,24 +9,22 @@ class OscarMovieTest extends MovieFakerTest {
 
     @RepeatedTest(100)
     void actor() {
-        assertThat(faker.oscarMovie().actor())
-            .is(CONDITION_FUNCTION.apply(c -> Character.isLetter(c) || c == ' ' || c == '.' || c == '-' || c == '(' || c == ')' || c == '\''));
+        assertThat(faker.oscarMovie().actor()).matches("\\P{Cc}+");
     }
 
-    @Test
+    @RepeatedTest(10)
     void movieName() {
         assertThat(isNullOrEmpty(faker.oscarMovie().movieName())).isFalse();
     }
 
-    @Test
+    @RepeatedTest(10)
     void quote() {
         assertThat(isNullOrEmpty(faker.oscarMovie().quote())).isFalse();
     }
 
     @RepeatedTest(100)
     void character() {
-        assertThat(faker.oscarMovie().character())
-            .is(CONDITION_FUNCTION.apply(c -> Character.isLetter(c) || c == ' ' || c == '.' || c == '-' || c == '\''));
+        assertThat(faker.oscarMovie().character()).matches("\\P{Cc}+");
     }
 
     @RepeatedTest(100)
