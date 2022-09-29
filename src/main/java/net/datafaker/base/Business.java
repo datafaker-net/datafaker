5cbcc9500ac89b061e5c40acbe7620e9ecd12028
Erik Pragt <erik.pragt@gmail.com>
2022-09-29 21:01:03 +1000

    Reformat code.

--- src/main/java/net/datafaker/base/Business.java
+++ src/main/java/net/datafaker/base/Business.java
@@ -7,23 +7,23 @@ import java.util.concurrent.TimeUnit;
  */
 public class Business extends AbstractProvider<BaseProviders> {
 
-	protected Business(BaseProviders faker) {
-		super(faker);
-	}
+    protected Business(BaseProviders faker) {
+        super(faker);
+    }
 
-	public String creditCardNumber() {
+    public String creditCardNumber() {
         return faker.numerify(resolve("business.credit_card_numbers_format"));
-	}
+    }
 
-	public String creditCardType() {
-		return resolve("business.credit_card_types");
-	}
+    public String creditCardType() {
+        return resolve("business.credit_card_types");
+    }
 
-	public String creditCardExpiry() {
+    public String creditCardExpiry() {
         return faker.date().future(365 * faker.number().numberBetween(1, 20), TimeUnit.DAYS, "YYYY-MM-dd");
-	}
+    }
 
-	public String securityCode() {
+    public String securityCode() {
         return faker.numerify(resolve("business.security_code"));
-	}
+    }
 }
