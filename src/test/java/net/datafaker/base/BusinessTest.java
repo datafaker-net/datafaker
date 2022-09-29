178eca263901755a1e52311587db44ff80e75239
Chirag Mangukia <chirag.mangukia@gmail.com>
2022-09-29 13:18:15 +0530

    Improved Credit card utility

--- src/test/java/net/datafaker/base/BusinessTest.java
+++ src/test/java/net/datafaker/base/BusinessTest.java
@@ -5,19 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BusinessTest extends BaseFakerTest<BaseFaker> {
-    @Test
-    void creditCardNumber() {
-        assertThat(faker.business().creditCardNumber()).isNotEmpty();
-    }
+	@Test
+	void creditCardNumber() {
+		assertThat(faker.business().creditCardNumber()).isNotEmpty();
+	}
 
-    @Test
-    void creditCardType() {
-        assertThat(faker.business().creditCardType()).isNotEmpty();
-    }
+	@Test
+	void creditCardType() {
+		assertThat(faker.business().creditCardType()).isNotEmpty();
+	}
 
-    @Test
-    void creditCardExpiry() {
-        assertThat(faker.business().creditCardExpiry()).isNotEmpty();
-    }
+	@Test
+	void creditCardExpiry() {
+		assertThat(faker.business().creditCardExpiry()).isNotEmpty();
+	}
 
+	@Test
+	void securityCode() {
+		assertThat(faker.business().securityCode()).isNotEmpty();
+	}
 }
