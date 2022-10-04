09160198b1be55b74faa017217d3dae71da224f7
Ilya Pantsyr <panilyau@gmail.com>
2022-10-04 14:41:52 +0300

    Add 'Fullmetal Alchemist: Brotherhood' provider

---
+++ src/test/java/net/datafaker/movie/FullmetalAlchemistTest.java
@@ -0,0 +1,24 @@
+package net.datafaker.movie;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FullmetalAlchemistTest extends MovieFakerTest {
+
+    @Test
+    void characterTest() {
+        assertThat(faker.fullMetalAlchemist().character()).isNotEmpty();
+    }
+
+    @Test
+    void cityTest() {
+        assertThat(faker.fullMetalAlchemist().city()).isNotEmpty();
+    }
+
+    @Test
+    void countryTest() {
+        assertThat(faker.fullMetalAlchemist().country()).isNotEmpty();
+    }
+
+}
