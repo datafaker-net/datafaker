9b7ffd82a8b899ee1de682b84365335fd0408bec
Rick M <kingthorin@users.noreply.github.com>
2023-01-30 00:44:25 -0500

    Garment Size Test Tweak (#657)
    Check the the value returned is one of the expected values, vs just not
    empty.
    
    Signed-off-by: kingthorin <kingthorin@users.noreply.github.com>

--- src/test/java/net/datafaker/providers/base/GarmentSizeTest.java
+++ src/test/java/net/datafaker/providers/base/GarmentSizeTest.java
@@ -1,6 +1,7 @@
 package net.datafaker.providers.base;
 
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,7 +9,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GarmentSizeTest extends BaseFakerTest<BaseFaker> {
 
     @Test
-    void size() {
-        assertThat(faker.garmentSize().size()).isNotEmpty();
+    void testSize() {
+        // Given
+        List<String> actualSizes = getActualSizes();
+        // When
+        String size = faker.garmentSize().size();
+        // Then
+        assertThat(actualSizes).anyMatch(size::equals);
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<String> getActualSizes() {
+        return (List<String>) faker.fakeValuesService().fetchObject("garments_sizes.sizes", faker.getContext());
     }
 }
