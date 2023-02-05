30da9e629c2b30657e3af13658ec27908c917fcd
HattoriHenzo <esombugma@gmail.com>
2023-02-05 11:49:28 -0500

    Add Brand faker description

--- src/main/java/net/datafaker/providers/base/Brand.java
+++ src/main/java/net/datafaker/providers/base/Brand.java
@@ -2,7 +2,10 @@ package net.datafaker.providers.base;
 
 
 /**
- * Generate random sport wearing brand, e.g. Adidas, Nike, Puma
+ * Generate random sport wearing brand, car brand or watch brand. Only generate brand by types of products.
+ * If some brands already exists in another faker, you can integrate this faker in the Brand faker
+ *
+ * @since 1.7.0
  */
 public class Brand extends AbstractProvider<BaseProviders> {
     protected Brand(BaseProviders faker) {
