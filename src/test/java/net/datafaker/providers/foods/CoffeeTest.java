34ea4733956b559180979a850242b68e1fd6e905
Sergey Nuyanzin <sergey.nuyanzin@aiven.io>
2023-02-10 14:03:53 +0100

    Apply comparison against yaml file content for tests within food package (#678)
    * Apply comparison against yaml file content for tests within food package
    
    * apply spotless

--- src/test/java/net/datafaker/providers/foods/CoffeeTest.java
+++ src/test/java/net/datafaker/providers/foods/CoffeeTest.java
@@ -3,44 +3,12 @@ package net.datafaker.providers.foods;
 import net.datafaker.providers.food.Coffee;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CoffeeTest extends FoodFakerTest {
-
-    @Test
-    void country() {
-        assertThat(faker.coffee().country()).isNotEmpty();
-    }
-
-    @Test
-    void region() {
-        assertThat(faker.coffee().region()).isNotEmpty();
-    }
-
-    @Test
-    void regionBrazil() {
-        assertThat(faker.coffee().region(Coffee.Country.BRAZIL)).isNotEmpty();
-    }
-
-    @Test
-    void variety() {
-        assertThat(faker.coffee().variety()).isNotEmpty();
-    }
-
-    @Test
-    void intensifier() {
-        assertThat(faker.coffee().intensifier()).isNotEmpty();
-    }
-
-    @Test
-    void body() {
-        assertThat(faker.coffee().body()).isNotEmpty();
-    }
-
-    @Test
-    void descriptor() {
-        assertThat(faker.coffee().descriptor()).isNotEmpty();
-    }
 
     @Test
     void notes() {
@@ -48,17 +16,40 @@ class CoffeeTest extends FoodFakerTest {
     }
 
     @Test
-    void name1() {
-        assertThat(faker.coffee().name1()).isNotEmpty();
-    }
-
-    @Test
-    void name2() {
-        assertThat(faker.coffee().name2()).isNotEmpty();
-    }
-
-    @Test
     void blendName() {
         assertThat(faker.coffee().blendName()).isNotEmpty();
+    }
+
+    private final Coffee coffee = getFaker().coffee();
+
+    @Override
+    protected Collection<TestSpec> providerListTest() {
+        return Arrays.asList(
+            TestSpec.of(coffee::body, "coffee.body"),
+            TestSpec.of(coffee::country, "coffee.country"),
+            TestSpec.of(coffee::descriptor, "coffee.descriptor"),
+            TestSpec.of(coffee::intensifier, "coffee.intensifier"),
+            TestSpec.of(coffee::name1, "coffee.name_1"),
+            TestSpec.of(coffee::name2, "coffee.name_2"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.BRAZIL), "coffee.regions.brazil"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.BURUNDI), "coffee.regions.burundi"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.COLOMBIA), "coffee.regions.colombia"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.COSTA_RICA), "coffee.regions.costa_rica"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.EL_SALVADOR), "coffee.regions.el_salvador"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.ETHIOPIA), "coffee.regions.ethiopia"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.GUATEMALA), "coffee.regions.guatemala"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.HONDURAS), "coffee.regions.honduras"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.INDIA), "coffee.regions.india"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.KENYA), "coffee.regions.kenya"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.MEXICO), "coffee.regions.mexico"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.NICARAGUA), "coffee.regions.nicaragua"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.PANAMA), "coffee.regions.panama"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.RWANDA), "coffee.regions.rwanda"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.SUMATRA), "coffee.regions.sumatra"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.TANZANIA), "coffee.regions.tanzania"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.UGANDA), "coffee.regions.uganda"),
+            TestSpec.of(() -> coffee.region(Coffee.Country.YEMEN), "coffee.regions.yemen"),
+            TestSpec.of(coffee::variety, "coffee.variety")
+        );
     }
 }
--- src/test/java/net/datafaker/providers/foods/DessertTest.java
+++ src/test/java/net/datafaker/providers/foods/DessertTest.java
@@ -1,23 +1,20 @@
 package net.datafaker.providers.foods;
 
-import org.junit.jupiter.api.Test;
+import net.datafaker.providers.food.Dessert;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Arrays;
+import java.util.Collection;
 
 class DessertTest extends FoodFakerTest {
 
-    @Test
-    void variety() {
-        assertThat(faker.dessert().variety()).matches("[A-Za-z ]+");
-    }
+    private final Dessert dessert = getFaker().dessert();
 
-    @Test
-    void topping() {
-        assertThat(faker.dessert().topping()).matches("[A-Za-z ]+");
-    }
-
-    @Test
-    void flavor() {
-        assertThat(faker.dessert().flavor()).matches("[A-Za-z ']+");
+    @Override
+    protected Collection<TestSpec> providerListTest() {
+        return Arrays.asList(
+            TestSpec.of(dessert::flavor, "dessert.flavor"),
+            TestSpec.of(dessert::topping, "dessert.topping"),
+            TestSpec.of(dessert::variety, "dessert.variety")
+        );
     }
 }
