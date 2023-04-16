ef39f476975bc4a9fd592b626d2aea49cc8dd9a7
Sergey Nuyanzin <snuyanzin@gmail.com>
2023-04-17 00:38:59 +0200

    Remove unused package private method and tests

--- src/test/java/net/datafaker/providers/base/DiseaseTest.java
+++ src/test/java/net/datafaker/providers/base/DiseaseTest.java
@@ -1,64 +1,19 @@
 package net.datafaker.providers.base;
 
-import org.junit.jupiter.api.RepeatedTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
 import java.util.Collection;
+import java.util.List;
 
 class DiseaseTest extends BaseFakerTest<BaseFaker> {
 
     @Override
     protected Collection<TestSpec> providerListTest() {
         Disease disease = faker.disease();
-        return List.of(TestSpec.of(disease::internalDisease, "disease.internal_disease", "[\\p{L}'()., 0-9-’]+"),
-                TestSpec.of(disease::neurology, "disease.neurology", "[\\p{L}'()., 0-9-’]+"),
-                TestSpec.of(disease::surgery, "disease.surgery", "[\\p{L}'()., 0-9-’]+"),
-                TestSpec.of(disease::paediatrics, "disease.paediatrics", "[\\p{L}'()., 0-9-’]+"),
-                TestSpec.of(disease::gynecologyAndObstetrics, "disease.gynecology_and_obstetrics", "[\\p{L}'()., 0-9-’]+"),
-                TestSpec.of(disease::ophthalmologyAndOtorhinolaryngology, "disease.ophthalmology_and_otorhinolaryngology", "[\\p{L}'()., 0-9-’]+"),
-                TestSpec.of(disease::dermatology, "disease.dermatology", "[\\p{L}'()., 0-9-’]+"));
-    }
-
-    void testInternalDiseaseWith10000Times() {
-        boolean isExist = false;
-        for (int i = 0; i < 10000; i++) {
-            String generateString = faker.disease().internalDisease();
-            if (generateString.equals("anaphylaxis")) {
-                isExist = true;
-            }
-        }
-        assertThat(isExist).isTrue();
-    }
-
-    @RepeatedTest(1000)
-    void testNeurologyWith1000Times() {
-        assertThat(faker.disease().neurology()).isNotEmpty();
-    }
-
-    @RepeatedTest(1000)
-    void testSurgeryWith1000Times() {
-        assertThat(faker.disease().surgery()).isNotEmpty();
-    }
-
-    @RepeatedTest(1000)
-    void testPaediatricsWith1000Times() {
-        assertThat(faker.disease().paediatrics()).isNotEmpty();
-    }
-
-    @RepeatedTest(1000)
-    void testGynecologyAndObstetricsWith1000Times() {
-        assertThat(faker.disease().gynecologyAndObstetrics()).isNotEmpty();
-    }
-
-    @RepeatedTest(1000)
-    void testOphthalmologyAndOtorhinolaryngologyWith1000Times() {
-        assertThat(faker.disease().ophthalmologyAndOtorhinolaryngology()).isNotEmpty();
-    }
-
-    @RepeatedTest(10000)
-    void testDermatoloryWith10000Times() {
-        assertThat(faker.disease().dermatology()).isNotEmpty();
+        return List.of(TestSpec.of(disease::internalDisease, "disease.internal_disease"),
+                TestSpec.of(disease::neurology, "disease.neurology"),
+                TestSpec.of(disease::surgery, "disease.surgery"),
+                TestSpec.of(disease::paediatrics, "disease.paediatrics"),
+                TestSpec.of(disease::gynecologyAndObstetrics, "disease.gynecology_and_obstetrics"),
+                TestSpec.of(disease::ophthalmologyAndOtorhinolaryngology, "disease.ophthalmology_and_otorhinolaryngology"),
+                TestSpec.of(disease::dermatology, "disease.dermatology"));
     }
 }
