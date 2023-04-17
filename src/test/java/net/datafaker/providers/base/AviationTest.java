410bf77863cd2d291e25cccba2ccf5d7b88d7af4
Evgeniy Lozovan <16156565+L-Evg@users.noreply.github.com>
2023-04-17 09:08:05 +0300

    Update src/test/java/net/datafaker/providers/base/AviationTest.java
    Co-authored-by: Sergey Nuyanzin <sergey.nuyanzin@aiven.io>

--- src/test/java/net/datafaker/providers/base/AviationTest.java
+++ src/test/java/net/datafaker/providers/base/AviationTest.java
@@ -23,7 +23,7 @@ class AviationTest extends BaseFakerTest<BaseFaker> {
 
     @Test
     void flight_default() {
-        assertThat(aviation.flight()).matches("[A-Z0-9]{2}[0-9]+");
+        assertThat(aviation.flight()).matches("[A-Z0-9]{2}\\d{1,4}");
     }
 
     @Test
