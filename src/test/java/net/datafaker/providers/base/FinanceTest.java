f4ff921ce2022cd8245fd5fe97b6ced8780b5918
Sergey Nuyanzin <snuyanzin@gmail.com>
2023-05-07 15:27:53 +0200

    Eliminate System.out.println, speed up Finance#usRoutingNumber

--- src/test/java/net/datafaker/providers/base/FinanceTest.java
+++ src/test/java/net/datafaker/providers/base/FinanceTest.java
@@ -86,15 +86,14 @@ class FinanceTest extends BaseFakerTest<BaseFaker> {
     @RepeatedTest(100)
     void usRoutingNumber() {
         String rtn = finance.usRoutingNumber();
-        System.out.println(rtn);
         assertThat(rtn).matches("\\d{9}");
         int check = 0;
         for (int index = 0; index < 3; index++) {
-            int pos = index * 3;
+            final int pos = index * 3;
             check += Character.getNumericValue(rtn.charAt(pos)) * 3;
             check += Character.getNumericValue(rtn.charAt(pos + 1)) * 7;
             check += Character.getNumericValue(rtn.charAt(pos + 2));
         }
-        assertThat(check % 10).isEqualTo(0);
+        assertThat(check % 10).isZero();
     }
 }
