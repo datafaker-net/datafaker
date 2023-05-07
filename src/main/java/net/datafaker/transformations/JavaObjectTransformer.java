8f49173f5a84b4002ef83d31b4759543a73415d5
Ilya Pantsyr <panilyau@gmail.com>
2023-05-07 19:06:48 +0300

    Cache record's constructor in JavaObjectTransformer

--- src/main/java/net/datafaker/transformations/JavaObjectTransformer.java
+++ src/main/java/net/datafaker/transformations/JavaObjectTransformer.java
@@ -2,6 +2,7 @@ package net.datafaker.transformations;
 
 import net.datafaker.sequence.FakeSequence;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.RecordComponent;
 import java.util.ArrayList;
@@ -17,40 +18,49 @@ import java.util.stream.Stream;
 public class JavaObjectTransformer implements Transformer<Object, Object> {
     private static final Map<Class<?>, Object> CLASS2RESULT = new IdentityHashMap<>();
     private static final Map<Schema<Object, ?>, Consumer<Object>> SCHEMA2CONSUMER = new IdentityHashMap<>();
+    private static final Map<Class<?>, Constructor<?>> CLASS2CONSTRUCTOR = new IdentityHashMap<>();
 
     @Override
     public Object apply(Object input, Schema<Object, ?> schema) {
         Class clazz;
-        Object result;
+        Object result = null;
         if (input instanceof Class) {
             clazz = (Class) input;
-            result = CLASS2RESULT.get(clazz);
         } else {
             clazz = input.getClass();
             result = input;
         }
 
-        if (result == null) {
-            if (clazz.isRecord()) {
+        if (clazz.isRecord()) {
+            Constructor<?> recordConstructor = CLASS2CONSTRUCTOR.get(clazz);
+            if (recordConstructor == null) {
                 Class<?>[] componentTypes = Arrays.stream(clazz.getRecordComponents())
                     .map(RecordComponent::getType)
                     .toArray(Class<?>[]::new);
 
-                final Field<Object, ?>[] fields = schema.getFields();
-                final Object[] values = new Object[fields.length];
-                for (int i = 0; i < fields.length; i++) {
-                    values[i] = fields[i].transform(result);
-                }
-
                 try {
-                    result = clazz.getDeclaredConstructor(componentTypes).newInstance(values);
-                } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
-                         InvocationTargetException e) {
+                    recordConstructor = clazz.getDeclaredConstructor(componentTypes);
+                    CLASS2CONSTRUCTOR.put(clazz, recordConstructor);
+                } catch (NoSuchMethodException e) {
                     throw new RuntimeException(e);
                 }
+            }
 
-                return result;
-            } else {
+            final Field<Object, ?>[] fields = schema.getFields();
+            final Object[] values = new Object[fields.length];
+            for (int i = 0; i < fields.length; i++) {
+                values[i] = fields[i].transform(result);
+            }
+
+            try {
+                result = recordConstructor.newInstance(values);
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+
+        } else {
+            result = CLASS2RESULT.get(clazz);
+            if (result == null) {
                 try {
                     result = clazz.getDeclaredConstructor().newInstance();
                     CLASS2RESULT.put(clazz, result);
@@ -59,30 +69,30 @@ public class JavaObjectTransformer implements Transformer<Object, Object> {
                     throw new RuntimeException(e);
                 }
             }
-        }
 
-        Consumer<Object> consumer = SCHEMA2CONSUMER.get(schema);
-        if (consumer == null) {
-            final Field<Object, ?>[] fields = schema.getFields();
-            final Map<String, java.lang.reflect.Field> name2ClassField = Stream.of(clazz.getDeclaredFields()).collect(
-                Collectors.toMap(java.lang.reflect.Field::getName, Function.identity()));
-            final java.lang.reflect.Field[] rFields = new java.lang.reflect.Field[fields.length];
-            for (int i = 0; i < fields.length; i++) {
-                rFields[i] = name2ClassField.get(fields[i].getName());
-                rFields[i].setAccessible(true);
-            }
-            consumer = classObject -> {
-                try {
-                    for (int i = 0; i < fields.length; i++) {
-                        rFields[i].set(classObject, fields[i].transform(classObject));
-                    }
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
+            Consumer<Object> consumer = SCHEMA2CONSUMER.get(schema);
+            if (consumer == null) {
+                final Field<Object, ?>[] fields = schema.getFields();
+                final Map<String, java.lang.reflect.Field> name2ClassField = Stream.of(clazz.getDeclaredFields()).collect(
+                    Collectors.toMap(java.lang.reflect.Field::getName, Function.identity()));
+                final java.lang.reflect.Field[] rFields = new java.lang.reflect.Field[fields.length];
+                for (int i = 0; i < fields.length; i++) {
+                    rFields[i] = name2ClassField.get(fields[i].getName());
+                    rFields[i].setAccessible(true);
                 }
-            };
-            SCHEMA2CONSUMER.put(schema, consumer);
+                consumer = classObject -> {
+                    try {
+                        for (int i = 0; i < fields.length; i++) {
+                            rFields[i].set(classObject, fields[i].transform(classObject));
+                        }
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                };
+                SCHEMA2CONSUMER.put(schema, consumer);
+            }
+            consumer.accept(result);
         }
-        consumer.accept(result);
         return result;
     }
 
