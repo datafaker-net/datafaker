0cfb469074903f4538d36e60ad818ef013dad0a1
Sergey Nuyanzin <snuyanzin@gmail.com>
2023-09-10 13:08:21 +0200

    SPeed up Lorem#sentence

--- src/main/java/net/datafaker/providers/base/Lorem.java
+++ src/main/java/net/datafaker/providers/base/Lorem.java
@@ -1,9 +1,10 @@
 package net.datafaker.providers.base;
 
+import net.datafaker.internal.helper.WordUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import static net.datafaker.internal.helper.WordUtils.capitalize;
 
 /**
  * @since 0.8.0
@@ -107,7 +108,15 @@ public class Lorem extends AbstractProvider<BaseProviders> {
      */
     public String sentence(int wordCount, int randomWordsToAdd) {
         int numberOfWordsToAdd = randomWordsToAdd == 0 ? 0 : faker.random().nextInt(randomWordsToAdd);
-        return capitalize(String.join(" ", words(wordCount + numberOfWordsToAdd)) + ".");
+        final int totalWordCount = wordCount + numberOfWordsToAdd;
+        StringBuilder sb = new StringBuilder();
+        if (totalWordCount > 0) {
+            sb.append(WordUtils.capitalize(word()));
+        }
+        for (int i = 1; i < totalWordCount; i++) {
+            sb.append(" ").append(word());
+        }
+        return sb + ".";
     }
 
     public List<String> sentences(int sentenceCount) {
