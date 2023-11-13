0a6cec6063d61cdc7e0ca57d82ab4237f50c282a
Sergey Nuyanzin <snuyanzin@gmail.com>
2023-11-13 21:26:20 +0100

    Current shoud not be a part of context

--- src/main/java/net/datafaker/service/RegExpContext.java
+++ src/main/java/net/datafaker/service/RegExpContext.java
@@ -6,19 +6,17 @@ import java.util.Objects;
 
 public class RegExpContext {
     private final String exp;
-    private final Object current;
     private final ProviderRegistration root;
     private final FakerContext context;
 
-    private RegExpContext(String exp, Object current, ProviderRegistration root, FakerContext context) {
+    private RegExpContext(String exp, ProviderRegistration root, FakerContext context) {
         this.exp = exp;
-        this.current = current;
         this.root = root;
         this.context = context;
     }
 
-    public static RegExpContext of(String exp, Object current, ProviderRegistration root, FakerContext context) {
-        return new RegExpContext(exp, current, root, context);
+    public static RegExpContext of(String exp, ProviderRegistration root, FakerContext context) {
+        return new RegExpContext(exp, root, context);
     }
 
     @Override
@@ -29,7 +27,6 @@ public class RegExpContext {
         RegExpContext that = (RegExpContext) o;
 
         if (!Objects.equals(exp, that.exp)) return false;
-        if (!Objects.equals(current, that.current)) return false;
         if (!Objects.equals(root, that.root)) return false;
         return Objects.equals(context, that.context);
     }
