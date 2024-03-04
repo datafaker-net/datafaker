f700fa92abe3017d50bad94e7b18c19b1b22cdc9
panilya <panilyau@gmail.com>
2024-03-04 23:33:53 +0200

    Use java.util.Currency in Money faker, delete money.yml

--- src/main/java/net/datafaker/providers/base/Money.java
+++ src/main/java/net/datafaker/providers/base/Money.java
@@ -1,5 +1,9 @@
 package net.datafaker.providers.base;
 
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+
 /**
  * Support for different kind of money currencies.
  *
@@ -7,25 +11,55 @@ package net.datafaker.providers.base;
  */
 public class Money extends AbstractProvider<BaseProviders> {
 
+    private final List<Currency> availableCurrencies;
+
     public Money(BaseProviders faker) {
         super(faker);
+        this.availableCurrencies = new ArrayList<>(Currency.getAvailableCurrencies());
     }
 
     /**
      * This method returns a currency value in a more descriptive manner like "United States dollar", etc.
      *
+     * @see Currency#getDisplayName()
      * @return detailed currency value.
      */
     public String currency() {
-        return resolve("money.currency");
+        int randomIndex = faker.random().nextInt(availableCurrencies.size());
+        return availableCurrencies.get(randomIndex).getDisplayName();
     }
 
     /**
-     * Method returns a randomly generated currency value like EUR, USD, etc.
+     * <p>Returns a random ISO 4217 currency code (ex. EUR, USD)</p>
      *
-     * @return currency code.
+     * @see Currency#getCurrencyCode()
+     * @return an ISO 4217 currency code
      */
     public String currencyCode() {
-        return resolve("money.code");
+        int randomIndex = faker.random().nextInt(availableCurrencies.size());
+        return availableCurrencies.get(randomIndex).getCurrencyCode();
     }
+
+    /**
+     * <p>Returns the 3 digit ISO 4217 numeric code of a random currency. Read more: </p>
+     *
+     * @see Currency#getNumericCodeAsString()
+     * @return an ISO 4217 currency numeric code
+     * @since 2.3.0
+     */
+    public String currencyNumericCode() {
+        int randomIndex = faker.random().nextInt(availableCurrencies.size());
+        return availableCurrencies.get(randomIndex).getNumericCodeAsString();
+    }
+
+    /**
+     * @see Currency#getSymbol()
+     * @return an ISO 4217 currency symbol
+     * @since 2.3.0
+     */
+    public String currencySymbol() {
+        int randomIndex = faker.random().nextInt(availableCurrencies.size());
+        return availableCurrencies.get(randomIndex).getSymbol();
+    }
+
 }
