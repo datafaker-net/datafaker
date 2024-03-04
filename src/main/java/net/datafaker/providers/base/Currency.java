0c28391c0ccb35f9e39b0b9bba03b40e7339427e
panilya <panilyau@gmail.com>
2024-03-04 23:32:09 +0200

    Deprecate Currency faker

--- src/main/java/net/datafaker/providers/base/Currency.java
+++ src/main/java/net/datafaker/providers/base/Currency.java
@@ -2,18 +2,25 @@ package net.datafaker.providers.base;
 
 /**
  * @since 0.8.0
+ * @deprecated since 2.3.0. For removal in 3.0.0 version. This faker is deprecated due to the existence
+ * of duplicate {@link Money} faker with similar methods. Use {@link Money} instead.
  */
+@Deprecated(since = "2.3.0", forRemoval = true)
 public class Currency extends AbstractProvider<BaseProviders> {
 
     public Currency(BaseProviders faker) {
         super(faker);
     }
 
+    /**
+     * @deprecated since 2.3.0. For removal in 3.0.0 version. Use {@link Money#currency} instead.
+     */
     public String name() {
         return resolve("currency.name");
     }
 
     /**
+     * @deprecated since 2.3.0. For removal in 3.0.0 version. Use {@link Money#currencyCode()} instead.
      * @return an alphabetic currency code (ex. USD)
      */
     public String code() {
@@ -22,6 +29,8 @@ public class Currency extends AbstractProvider<BaseProviders> {
 
     /**
      * <p>Returns an active ISO 4217 three-digit numeric currency code as of 1 April 2022</p>
+     *
+     * @deprecated since 2.3.0. For removal in 3.0.0 version. Use {@link Money#currencyNumericCode()} instead.
      *
      * @return an ISO 4217 currency numeric code
      * @since 2.2.0
