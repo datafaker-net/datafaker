80a3854de38ad31d5ba4ffbfd8ef2a32c5c0f0aa
panilya <panilyau@gmail.com>
2024-03-04 23:34:36 +0200

    Use java.util.Currency in Country faker in currency related methods

--- src/main/java/net/datafaker/providers/base/Country.java
+++ src/main/java/net/datafaker/providers/base/Country.java
@@ -1,14 +1,21 @@
 package net.datafaker.providers.base;
 
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+
 /**
  * @since 0.8.0
  */
 public class Country extends AbstractProvider<BaseProviders> {
     private final String flagUrl;
 
+    private final List<Currency> availableCurrencies;
+
     protected Country(BaseProviders faker) {
         super(faker);
         this.flagUrl = "https://flags.fmcdn.net/data/flags/w580/";
+        this.availableCurrencies = new ArrayList<>(Currency.getAvailableCurrencies());
     }
 
     public String flag() {
@@ -27,12 +34,22 @@ public class Country extends AbstractProvider<BaseProviders> {
         return resolve("country.capital");
     }
 
+    /**
+     * @see Currency#getDisplayName()
+     * @return a random detailed ISO 4217 currency display name
+     */
     public String currency() {
-        return resolve("country.currency");
+        int randomIndex = faker.random().nextInt(availableCurrencies.size());
+        return availableCurrencies.get(randomIndex).getDisplayName();
     }
 
+    /**
+     * @see Currency#getCurrencyCode()
+     * @return an ISO 4217 currency code
+     */
     public String currencyCode() {
-        return resolve("country.currency_code");
+        int randomIndex = faker.random().nextInt(availableCurrencies.size());
+        return availableCurrencies.get(randomIndex).getCurrencyCode();
     }
 
     public String name() {
