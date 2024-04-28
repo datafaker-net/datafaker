7a9ca5efe148a6c13da2d803a2670b5cf269aabb
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2024-04-29 00:13:54 +0200

    chore: format module-info to use spaces instead of tabs

--- src/moditect/module-info.java
+++ src/moditect/module-info.java
@@ -1,22 +1,22 @@
 open module net.datafaker {
-	requires java.sql;
-	requires java.management;
+    requires java.sql;
+    requires java.management;
 
-	exports net.datafaker;
-	exports net.datafaker.annotations;
-	exports net.datafaker.formats;
-	exports net.datafaker.idnumbers;
-	exports net.datafaker.idnumbers.pt.br;
-	exports net.datafaker.providers.base;
-	exports net.datafaker.providers.entertainment;
-	exports net.datafaker.providers.food;
+    exports net.datafaker;
+    exports net.datafaker.annotations;
+    exports net.datafaker.formats;
+    exports net.datafaker.idnumbers;
+    exports net.datafaker.idnumbers.pt.br;
+    exports net.datafaker.providers.base;
+    exports net.datafaker.providers.entertainment;
+    exports net.datafaker.providers.food;
     exports net.datafaker.providers.healthcare
-	exports net.datafaker.providers.sport;
-	exports net.datafaker.providers.videogame;
-	exports net.datafaker.sequence;
-	exports net.datafaker.service;
-	exports net.datafaker.service.files;
-	exports net.datafaker.transformations;
-	exports net.datafaker.transformations.sql;
+    exports net.datafaker.providers.sport;
+    exports net.datafaker.providers.videogame;
+    exports net.datafaker.sequence;
+    exports net.datafaker.service;
+    exports net.datafaker.service.files;
+    exports net.datafaker.transformations;
+    exports net.datafaker.transformations.sql;
     exports net.datafaker.shaded.snakeyaml;
 }
