5ea294688d72bf3fc1930e76baf91d4c6d36bc2c
Valery Yatsynovich <valery_yatsynovich@epam.com>
2024-05-01 13:22:58 +0300

    Fix inception version of `Pronouns` data provider (#1182)

--- src/main/java/net/datafaker/providers/base/Pronouns.java
+++ src/main/java/net/datafaker/providers/base/Pronouns.java
@@ -2,7 +2,7 @@ package net.datafaker.providers.base;
 
 /**
  * This class generates gender neutral pronouns
- * @since 2.1.0
+ * @since 2.2.0
  */
 public class Pronouns extends AbstractProvider<BaseProviders> {
 
