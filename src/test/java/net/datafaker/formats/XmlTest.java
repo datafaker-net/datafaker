75bac23a03d2ef9d538d08855f5fdd770e66f304
Sergey Nuyanzin <snuyanzin@gmail.com>
2024-06-01 22:02:45 +0200

    Drop deprecated Xml

--- src/test/java/net/datafaker/formats/XmlTest.java
+++ src/test/java/net/datafaker/formats/XmlTest.java
@@ -12,7 +12,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -158,63 +159,6 @@ class XmlTest {
         assertThat(xml).isNotEmpty();
         int numberOfLines = getNumberOfLines(xml);
         assertThat(numberOfLines).isEqualTo(23);
-    }
-
-    @ParameterizedTest
-    @MethodSource("generateTestXmlPretty")
-    void xmlPrettyTest(Xml.XmlNode xmlNode, String expected) {
-        Xml xml = new Xml(xmlNode);
-        assertThat(xml.generate(true)).isEqualTo(expected);
-    }
-
-    private static Stream<Arguments> generateTestXmlPretty() {
-        return Stream.of(
-            Arguments.of(new Xml.XmlNode("root", Collections.emptyList()), "<root/>"),
-            Arguments.of(new Xml.XmlNode("root", "value"), "<root>value</root>"),
-            Arguments.of(new Xml.XmlNode("root",
-                    map(entry("attribute1", "value1"), entry("attribute2", "value2")), "value"),
-                "<root attribute1=\"value1\" attribute2=\"value2\">value</root>"),
-            Arguments.of(new Xml.XmlNode("root",
-                    map(entry("attribute1", "value1"), entry("attribute2", "value2")),
-                List.of(new Xml.XmlNode("child", "value"))),
-                "<root attribute1=\"value1\" attribute2=\"value2\">" + System.lineSeparator() + "    <child>value</child>" + System.lineSeparator() + "</root>"),
-            Arguments.of(new Xml.XmlNode("root", "<> value\""), "<root>&lt;&gt; value&quot;</root>")
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("generateTestXml")
-    void xmlTest(Xml.XmlNode xmlNode, String expected) {
-        Xml xml = new Xml(xmlNode);
-        assertThat(xml.generate()).isEqualTo(expected);
-    }
-
-    private static Stream<Arguments> generateTestXml() {
-        return Stream.of(
-            Arguments.of(new Xml.XmlNode("root", Collections.emptyList()), "<root/>"),
-            Arguments.of(new Xml.XmlNode("root", "value"), "<root>value</root>"),
-            Arguments.of(new Xml.XmlNode("root",
-                    map(entry("attribute1", "value1"), entry("attribute2", "value2")), "value"),
-                "<root attribute1=\"value1\" attribute2=\"value2\">value</root>"),
-            Arguments.of(new Xml.XmlNode("root",
-                    map(entry("attribute1", "value1"), entry("attribute2", "value2")),
-                List.of(new Xml.XmlNode("child", "value"))),
-                "<root attribute1=\"value1\" attribute2=\"value2\"><child>value</child></root>"),
-            Arguments.of(new Xml.XmlNode("root", "<> value\""), "<root>&lt;&gt; value&quot;</root>")
-        );
-    }
-
-    private static Map.Entry<String, String> entry(String key, String value) {
-        return new AbstractMap.SimpleEntry<>(key, value);
-    }
-
-    @SafeVarargs
-    private static Map<String, String> map(Map.Entry<String, String>... entries) {
-        Map<String, String> map = new LinkedHashMap<>();
-        for (Map.Entry<String, String> entry : entries) {
-            map.put(entry.getKey(), entry.getValue());
-        }
-        return map;
     }
 
     private static int getNumberOfLines(String xml) {
