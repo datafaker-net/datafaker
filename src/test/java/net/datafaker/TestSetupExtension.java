8a0479b9bf22445049fcb66c4763a9c8b50ed08e
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-07-21 18:08:48 +0300

    #1306 setup FINE logging in tests
    * by default, FINE logs are NOT written to console, BUT
    * if some test failed, all its FINE logs are written to console.
    
    Thus we can investigate flaky tests.

---
+++ src/test/java/net/datafaker/TestSetupExtension.java
@@ -0,0 +1,80 @@
+package net.datafaker;
+
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.opentest4j.TestAbortedException;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.TimeZone;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import static java.util.logging.LogManager.getLogManager;
+
+public class TestSetupExtension implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
+  private static final AtomicLong counter = new AtomicLong(1000);
+  private static final ExtensionContext.Namespace namespace = ExtensionContext.Namespace.create(TestSetupExtension.class);
+
+  @Override
+  public void beforeAll(ExtensionContext context) throws IOException, URISyntaxException {
+      String loggingConfig = new File(getClass().getResource("/logging.properties").toURI()).getAbsolutePath();
+      System.setProperty("java.util.logging.config.file", loggingConfig);
+      getLogManager().readConfiguration();
+
+      Logger.getLogger(context.getDisplayName()).info(() -> "Starting tests (%s)".formatted(memory()));
+  }
+
+  @Override
+  public void afterAll(ExtensionContext context) {
+      Logger.getLogger(context.getDisplayName()).info(() -> "Finished tests - %s (%s)".formatted(verdict(context), memory()));
+  }
+
+  @Override
+  public void beforeEach(ExtensionContext context) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kaohsiung"));
+    long testId = counter.incrementAndGet();
+    context.getStore(namespace).put("test-id", testId);
+    context.getStore(namespace).put("original-thread-name", Thread.currentThread().getName());
+
+    Thread.currentThread().setName("%s.%s#%d#".formatted(context.getRequiredTestClass().getSimpleName(), context.getRequiredTestMethod().getName(), testId));
+
+    Logger.getLogger(context.getRequiredTestClass().getName()).info(() -> "starting %s (%s)...".formatted(context.getDisplayName(), memory()));
+    ThreadLocalLogHandler.start();
+  }
+
+  @Override
+  public void afterEach(ExtensionContext context) {
+      ThreadLocalLogHandler.finish(context.getExecutionException().isPresent());
+
+      Logger.getLogger(context.getRequiredTestClass().getName())
+          .info(() -> "finished %s - %s (%s)".formatted(context.getDisplayName(), verdict(context), memory()));
+
+      String originalThreadName = context.getStore(namespace).remove("original-thread-name", String.class);
+      Thread.currentThread().setName(originalThreadName);
+      context.getStore(namespace).remove("test-id");
+      TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kaohsiung"));
+  }
+
+  private String verdict(ExtensionContext context) {
+    return context.getExecutionException().isPresent() ?
+      (context.getExecutionException().get() instanceof TestAbortedException ? "skipped" : "NOK") :
+      "OK";
+  }
+
+  private String memory() {
+    long freeMemory = Runtime.getRuntime().freeMemory();
+    long maxMemory = Runtime.getRuntime().maxMemory();
+    long totalMemory = Runtime.getRuntime().totalMemory();
+    long usedMemory = totalMemory - freeMemory;
+    return "memory used:" + mb(usedMemory) + ", free:" + mb(freeMemory) + ", total:" + mb(totalMemory) + ", max:" + mb(maxMemory);
+  }
+
+  private long mb(long bytes) {
+    return bytes / 1024 / 1024;
+  }
+}
