2c31e603dfccc70b6158c44560de84b071d6bbb7
Andrey <andrey1010102008@gmail.com>
2024-08-28 20:12:08 +0300

    Added tier() method in Dota2, updated data in dota2.yml, updated test dota2HeroQuote()

--- src/test/java/net/datafaker/providers/videogame/Dota2Test.java
+++ src/test/java/net/datafaker/providers/videogame/Dota2Test.java
@@ -2,98 +2,24 @@ package net.datafaker.providers.videogame;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Random;
-import java.util.Set;
+import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Dota2Test extends VideoGameFakerTest {
-    private final Dota2 dota2 = getFaker().dota2();
 
-    // All heroes that are currently contained within the provider
-    private final Set<String> availableHeroes = Set.of(
-        "abaddon",
-        "alchemist",
-        "axe",
-        "beastmaster",
-        "brewmaster",
-        "bristleback",
-        "centaur",
-        "chaos_knight",
-        "clockwerk",
-        "dawn_breaker",
-        "doom",
-        "dragon_knight",
-        "earth_spirit",
-        "earthshaker",
-        "elder_titan",
-        "huskar",
-        "io",
-        "kunkka",
-        "legion_commander",
-        "lifestealer",
-        "lycan",
-        "magnus",
-        "meepo",
-        "night_stalker",
-        "omniknight",
-        "primal_beast",
-        "phoenix",
-        "pudge",
-        "sand_king",
-        "slardar",
-        "spirit_breaker",
-        "sven",
-        "tidehunter",
-        "timbersaw",
-        "tiny",
-        "treant_protector",
-        "tusk",
-        "underlord",
-        "undying",
-        "wraith_king",
-        "tinker",
-        "muerta",
-        "arc_warden",
-        "batrider",
-        "visage",
-        "broodmother",
-        "techies",
-        "viper",
-        "rubick",
-        "morphling",
-        "outworld_destroyer",
-        "puck",
-        "chen",
-        "oracle",
-        "warlock",
-        "bane",
-        "spectre",
-        "clinkz",
-        "shadow_demon",
-        "pugna",
-        "marci",
-        "enchantress",
-        "jakiro",
-        "weaver",
-        "naga_siren"
-    );
+    private final Dota2 dota2 = getFaker().dota2();
 
     @Test
     void dota2HeroQuote() {
-        List<String> availableHeroList = new ArrayList<>(availableHeroes);
-
-        Random random = new Random();
-
         for (int i = 0; i < 5; i++) {
-            int randomIndex = random.nextInt(availableHeroList.size());
+            String randomHero = dota2.hero();
 
-            String randomHero = availableHeroList.get(randomIndex);
+            String snakeCasedRandomHero = randomHero.replace(" ", "_").toLowerCase(Locale.ENGLISH);
 
-            String randomHeroQuote = dota2.heroQuote(randomHero);
+            String randomHeroQuote = dota2.heroQuote(snakeCasedRandomHero);
 
             assertThat(randomHeroQuote).isNotEmpty();
         }
@@ -108,7 +34,9 @@ class Dota2Test extends VideoGameFakerTest {
             TestSpec.of(dota2::building, "games.dota2.building"),
             TestSpec.of(dota2::hero, "games.dota2.hero"),
             TestSpec.of(dota2::item, "games.dota2.item"),
+            TestSpec.of(dota2::neutralItem, "games.dota2.neutral_item"),
             TestSpec.of(dota2::team, "games.dota2.team"),
+            TestSpec.of(dota2::tier, "games.dota2.tier"),
             TestSpec.of(dota2::player, "games.dota2.player")
         );
     }
