b299646da978a6ef708a48289add9aec314d2cf6
George Vrettos <lukedevgr@gmail.com>
2024-11-03 01:15:57 +0200

    Add greek values for Football provider

--- src/test/java/net/datafaker/providers/sport/FootballTest.java
+++ src/test/java/net/datafaker/providers/sport/FootballTest.java
@@ -1,14 +1,19 @@
 package net.datafaker.providers.sport;
 
+import net.datafaker.providers.base.BaseFakerTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
+
 import java.util.List;
 import java.util.Collection;
+import java.util.Locale;
 
 class FootballTest extends SportFakerTest {
 
     private final Football football = getFaker().football();
 
-    @Override
-    protected Collection<TestSpec> providerListTest() {
+    private static Collection<TestSpec> getProviderListTests(Football football) {
         return List.of(
             TestSpec.of(football::coaches, "football.coaches"),
             TestSpec.of(football::competitions, "football.competitions"),
@@ -16,5 +21,30 @@ class FootballTest extends SportFakerTest {
             TestSpec.of(football::positions, "football.positions"),
             TestSpec.of(football::teams, "football.teams")
         );
+    }
+
+    @Override
+    protected Collection<TestSpec> providerListTest() {
+        return getProviderListTests(football);
+    }
+
+    @Nested
+    class FootballInGreekTest extends BaseFakerTest<SportFaker> {
+
+        @BeforeAll
+        void setup() {
+            this.setFaker(new SportFaker(new Locale("el", "GR")));
+        }
+
+        @AfterAll
+        void reset() {
+            this.setFaker(new SportFaker());
+        }
+
+        @Override
+        protected Collection<TestSpec> providerListTest() {
+            Football football = faker.football();
+            return getProviderListTests(football);
+        }
     }
 }
