add0fe175c97a8b9b48d7560ed4191affe56abf6
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-11-18 11:34:50 +0530

    add support for movie name

--- src/main/java/net/datafaker/providers/entertainment/Movie.java
+++ src/main/java/net/datafaker/providers/entertainment/Movie.java
@@ -11,12 +11,12 @@ public class Movie extends AbstractProvider<EntertainmentProviders> {
         super(faker);
     }
 
-    /**
-     * This method generates a random quote from a movie.
-     *
-     * @return a string of quote from a movie.
-     */
     public String quote() {
         return resolve("movie.quote");
     }
+    
+    public String name() {
+        return resolve("movie.name");
+    }
+    
 }
