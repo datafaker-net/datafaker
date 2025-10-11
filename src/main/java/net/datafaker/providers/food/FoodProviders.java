854d99cabff81778f9625448ce5ee26b1fef06a4
Sergejs Visockis <sergei.visotsky@gmail.com>
2025-10-11 15:35:27 +0200

    add Apple types and colours

--- src/main/java/net/datafaker/providers/food/FoodProviders.java
+++ src/main/java/net/datafaker/providers/food/FoodProviders.java
@@ -3,6 +3,10 @@ package net.datafaker.providers.food;
 import net.datafaker.providers.base.ProviderRegistration;
 
 public interface FoodProviders extends ProviderRegistration {
+    default Apple apple() {
+        return getProvider(Apple.class, Apple::new);
+    }
+
     default Beer beer() {
         return getProvider(Beer.class, Beer::new);
     }
